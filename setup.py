6f0c03483645fe72ad1300bdadbfb1bc5241c6fb
Ludwig Schwardt <ludwig@ska.ac.za>
2009-06-11 10:30:04 +0200

    Added build boilerplate.
    git-svn-id: https://katfs.kat.ac.za/svnDS/code/katpoint/trunk@1758 b56c7a7d-ac47-0410-a905-b6f7de78896a

---
+++ setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+setup (
+    name = "katpoint",
+    version = "trunk",
+    description = "Karoo Array Telescope pointing coordinate library'",
+    author = "Ludwig Schwardt",
+    author_email = "ludwig@ska.ac.za",
+    packages = find_packages(),
+    url='http://ska.ac.za/',
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: Other/Proprietary License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Scientific/Engineering :: Astronomy",
+    ],
+    platforms = [ "OS Independent" ],
+    keywords="kat kat7 ska",
+    zip_safe = False,
+    # Bitten Test Suite
+    test_suite = "katpoint.test.suite",
+)
