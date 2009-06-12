f0860c5c24472f2834ce7394b29caefd14b203cf
Ludwig Schwardt <ludwig@ska.ac.za>
2009-06-12 16:55:48 +0200

    Added catalogue filtering. This can be done on tags, flux, elevation and proximity to other targets. The names() method is now implemented via iterators.
    git-svn-id: https://katfs.kat.ac.za/svnDS/code/katpoint/trunk@1775 b56c7a7d-ac47-0410-a905-b6f7de78896a

--- katpoint/catalogue.py
+++ katpoint/catalogue.py
@@ -1,10 +1,13 @@
 """Target catalogue."""
 
 import logging
+import time
 
 import ephem
+import numpy as np
 
-from .target import construct_target, Target
+from .target import construct_target, Target, separation
+from .ephem_extra import rad2deg
 
 logger = logging.getLogger("katpoint.catalogue")
 
@@ -48,6 +51,18 @@ class Catalogue(object):
         """Iterate over targets in catalogue."""
         return iter(self.targets)
     
+    def iternames(self):
+        """Iterator over known target names in catalogue which can be searched for.
+        
+        There are potentially more names than targets in the catalogue, as the
+        same target can have many names.
+        
+        """
+        for target in self.targets:
+            yield target.name
+            for alias in target.aliases:
+                yield alias
+    
     def names(self):
         """List of known target names in catalogue which can be searched for.
         
@@ -55,11 +70,7 @@ class Catalogue(object):
         same target can have many names.
         
         """
-        names = []
-        for target in self.targets:
-            names.append(target.name)
-            names.extend(target.aliases)
-        return names
+        return [name for name in self.iternames()]
     
     def add(self, targets, tags=None):
         """Add targets to catalogue.
@@ -125,6 +136,88 @@ class Catalogue(object):
                 self.lookup.pop(_hash(alias))
             self.targets.remove(target)
     
-    def filter(self, flux, az, el, tags):
-        pass
+    def filter(self, tags=None, flux_Jy_limit=None, flux_freq_Hz=None, el_deg_limit=None,
+               dist_deg_limit=None, proximity_targets=None, antenna=None, timestamp=None):
+        """Filter catalogue on various criteria.
         
+        Parameters
+        ----------
+        tags : string, or sequence of strings, optional
+            Tag or list of tags which targets should have. Tags prepended with
+            a tilde (~) indicate tags which targets should *not* have. If None
+            or an empty list, all tags are accepted.
+        flux_Jy_limit : float or sequence of 2 floats, optional
+            Allowed flux density range, in Jy. If this is a single number, it is
+            the lower limit, otherwise it takes the form [lower, upper]. If None,
+            any flux density is accepted.
+        flux_freq_Hz : float, optional
+            Frequency at which to evaluate the flux density, in Hz (required for
+            flux filter)
+        el_deg_limit : float or sequence of 2 floats, optional
+            Allowed elevation range, in degrees. If this is a single number, it
+            is the lower limit, otherwise it takes the form [lower, upper].
+            If None, any elevation is accepted.
+        dist_deg_limit : float or sequence of 2 floats, optional
+            Allowed range of angular distance to proximity targets, in degrees.
+            If this is a single number, it is the lower limit, otherwise it
+            takes the form [lower, upper]. If None, any distance is accepted.
+        proximity_targets : :class:`Target` object, or sequence of objects
+            Target or list of targets used in proximity filter
+        antenna : :class:`katpoint.Antenna` object, optional
+            Antenna which points at targets (needed for position-based filters)
+        timestamp : float, optional
+            Timestamp at which to evaluate target positions, in seconds since
+            Unix epoch. If None, the current time is used.
+        
+        Returns
+        -------
+        subset : :class:`Catalogue` object
+            Filtered catalogue
+        
+        """
+        # Put targets in a numpy array to allow use of fancy indexing
+        targets = np.array(self.targets)
+        if not flux_Jy_limit is None:
+            if np.isscalar(flux_Jy_limit):
+                flux_Jy_limit = [flux_Jy_limit, np.inf]
+            if not flux_freq_Hz:
+                raise ValueError('Please specify frequency at which to measure flux density')
+            flux = np.array([target.flux_density(flux_freq_Hz) for target in targets])
+            targets = targets[(flux >= flux_Jy_limit[0]) & (flux <= flux_Jy_limit[1])]
+        
+        if not tags is None:
+            if isinstance(tags, basestring):
+                tags = [tags]
+            desired_tags = set([tag for tag in tags if tag[0] != '~'])
+            undesired_tags = set([tag[1:] for tag in tags if tag[0] == '~'])
+            if desired_tags:
+                targets = [target for target in targets if set(target.tags) & desired_tags]
+            if undesired_tags:
+                targets = np.array([target for target in targets if not (set(target.tags) & undesired_tags)])
+        
+        if not el_deg_limit is None:
+            if np.isscalar(el_deg_limit):
+                el_deg_limit = [el_deg_limit, 90.0]
+            if antenna is None:
+                raise ValueError('Antenna object needed to calculate target elevation')
+            if timestamp is None:
+                timestamp = time.time()
+            el_deg = np.array([rad2deg(antenna.point(target, timestamp)[1]) for target in targets])
+            targets = targets[(el_deg >= el_deg_limit[0]) & (el_deg <= el_deg_limit[1])]
+        
+        if (not dist_deg_limit is None) and (not proximity_targets is None):
+            if np.isscalar(dist_deg_limit):
+                dist_deg_limit = [dist_deg_limit, 180.0]
+            if isinstance(proximity_targets, Target):
+                proximity_targets = [proximity_targets]
+            if antenna is None:
+                raise ValueError('Antenna object needed to calculate angular separation of targets')
+            if timestamp is None:
+                timestamp = time.time()
+            dist = np.array([[rad2deg(separation(target1, target2, antenna, timestamp))
+                              for target2 in proximity_targets] for target1 in targets])
+            print dist
+            targets = targets[(dist >= dist_deg_limit[0]).all(axis=1) &
+                              (dist <= dist_deg_limit[1]).all(axis=1)]
+            
+        return Catalogue(targets, add_specials=False)
