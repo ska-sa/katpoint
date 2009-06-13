0b45967cd601b182ec68f05ca0adbd76a552a520
Ludwig Schwardt <ludwig@ska.ac.za>
2009-06-13 21:29:33 +0200

    Moved top-level projection routines out of __init__ to projection module.
    git-svn-id: https://katfs.kat.ac.za/svnDS/code/katpoint/trunk@1780 b56c7a7d-ac47-0410-a905-b6f7de78896a

--- katpoint/projection.py
+++ katpoint/projection.py
@@ -503,12 +503,102 @@ def plane_to_sphere_stg(az0, el0, x, y):
 #--------------------------------------------------------------------------------------------------
 
 # Maps projection code to appropriate function
-sphere_to_plane = {'SIN' : sphere_to_plane_sin,
-                   'TAN' : sphere_to_plane_tan,
-                   'ARC' : sphere_to_plane_arc,
-                   'STG' : sphere_to_plane_stg}
+sphere_to_plane_select = {'SIN' : sphere_to_plane_sin,
+                          'TAN' : sphere_to_plane_tan,
+                          'ARC' : sphere_to_plane_arc,
+                          'STG' : sphere_to_plane_stg}
 
-plane_to_sphere = {'SIN' : plane_to_sphere_sin,
-                   'TAN' : plane_to_sphere_tan,
-                   'ARC' : plane_to_sphere_arc,
-                   'STG' : plane_to_sphere_stg}
+plane_to_sphere_select = {'SIN' : plane_to_sphere_sin,
+                          'TAN' : plane_to_sphere_tan,
+                          'ARC' : plane_to_sphere_arc,
+                          'STG' : plane_to_sphere_stg}
+
+#--------------------------------------------------------------------------------------------------
+#--- Top-level projection routines
+#--------------------------------------------------------------------------------------------------
+
+def sphere_to_plane(antenna, target, az, el, timestamps, projection_type='ARC', coord_system='azel'):
+    """Project spherical coordinates to plane with target position as reference.
+    
+    This is a convenience function that projects spherical coordinates to a 
+    plane with the target position as the origin of the plane. The function is
+    vectorised and can operate on single or multiple timestamps, as well as
+    single or multiple coordinate vectors. The spherical coordinates may be
+    (az, el) or (ra, dec), and the projection type can also be specified.
+    
+    Parameters
+    ----------
+    antenna : :class:`antenna.Antenna` object
+        Antenna pointing at target
+    target : :class:`target.Target` object
+        Target object serving as origin for projected coordinates
+    az : float or array
+        Azimuth or right ascension, in radians
+    el : float or array
+        Elevation or declination, in radians
+    timestamps : float or array
+        Local timestamp(s) in seconds since Unix epoch
+    projection_type : {'ARC', 'SIN', 'TAN', 'STG'}, optional
+        Type of spherical projection
+    coord_system : {'azel', 'radec'}, optional
+        Spherical coordinate system
+    
+    Returns
+    -------
+    x : float or array
+        Azimuth-like coordinate(s) on plane, in radians
+    y : float or array
+        Elevation-like coordinate(s) on plane, in radians
+    
+    """
+    if coord_system == 'radec':
+        # The target (ra, dec) coordinates will serve as reference point on the sphere
+        ref_ra, ref_dec = target.radec(antenna, timestamps)
+        return sphere_to_plane_select[projection_type](ref_ra, ref_dec, az, el)
+    else:
+        # The target (az, el) coordinates will serve as reference point on the sphere
+        ref_az, ref_el = antenna.point(target, timestamps)
+        return sphere_to_plane_select[projection_type](ref_az, ref_el, az, el)
+
+def plane_to_sphere(antenna, target, x, y, timestamps, projection_type='ARC', coord_system='azel'):
+    """Deproject plane coordinates to sphere with target position as reference.
+    
+    This is a convenience function that deprojects plane coordinates to a
+    sphere with the target position as the origin of the plane. The function is
+    vectorised and can operate on single or multiple timestamps, as well as
+    single or multiple coordinate vectors. The spherical coordinates may be
+    (az, el) or (ra, dec), and the projection type can also be specified.
+    
+    Parameters
+    ----------
+    antenna : :class:`antenna.Antenna` object
+        Antenna pointing at target
+    target : :class:`target.Target` object
+        Target object serving as origin for projected coordinates
+    x : float or array
+        Azimuth-like coordinate(s) on plane, in radians
+    y : float or array
+        Elevation-like coordinate(s) on plane, in radians
+    timestamps : float or array
+        Local timestamp(s) in seconds since Unix epoch
+    projection_type : {'ARC', 'SIN', 'TAN', 'STG'}, optional
+        Type of spherical projection
+    coord_system : {'azel', 'radec'}, optional
+        Spherical coordinate system
+    
+    Returns
+    -------
+    az : float or array
+        Azimuth or right ascension, in radians
+    el : float or array
+        Elevation or declination, in radians
+    
+    """
+    if coord_system == 'radec':
+        # The target (ra, dec) coordinates will serve as reference point on the sphere
+        ref_ra, ref_dec = target.radec(antenna, timestamps)
+        return plane_to_sphere_select[projection_type](ref_az, ref_el, x, y)
+    else:
+        # The target (az, el) coordinates will serve as reference point on the sphere
+        ref_az, ref_el = antenna.point(target, timestamps)
+        return plane_to_sphere_select[projection_type](ref_az, ref_el, x, y)
