5d86f785011ffc288b4373faaff1bd4f340da0f0
Ludwig Schwardt <ludwig@ska.ac.za>
2010-03-13 02:48:47 +0200

    Added a script that extracts Parkes catalogue sources with L-band flux >= 10 Jy into a katpoint catalogue format (i.e. target description strings). The original file is a VOTable downloaded from VizieR, and the CSV file is the end result.
    git-svn-id: https://katfs.kat.ac.za/svnDS/code/katpoint/trunk@4804 b56c7a7d-ac47-0410-a905-b6f7de78896a

---
+++ scripts/parkes_sources.py
@@ -0,0 +1,93 @@
+#! /usr/bin/python
+#
+# Tool that extracts bright sources from Parkes Radio Sources Catalogue (PKSCAT90).
+#
+# This builds a katpoint catalogue from the included pkscat90_S1410_min_10Jy.vot file.
+# This file is obtained as follows:
+#
+# - Visit the VizieR web site: http://vizier.u-strasbg.fr/cgi-bin/VizieR
+# - Under "Direct access to Catalogues from Name or Designation", enter "pkscat90"
+# - Click on "VIII/15/pkscat90" ("The Catalogue")
+# - Select at least the following fields:
+#     Jname Alias Ident RA2000 DE2000
+#     all flux densities (S80 S178 S408 S1410 S2700 S5000 S8400 S22000)
+# - Add a constraint to S1410 of ">=10", to select sources with L-Band flux above 10 Jy
+# - Select "VOTable" output layout and click on "Submit query"
+# - This downloads a file named vizier_votable.vot
+# - Open this in a text editor and delete the following section around lines 12-14
+#   (The 'DEFINITIONS' element is deprecated in VOTable 1.1):
+#   <DEFINITIONS>
+#     <COOSYS ID="J2000" system="eq_FK5" equinox="J2000"/>
+#   </DEFINITIONS>
+# - Save file as pkscat90_S1410_min_10Jy.vot
+#
+# Thereafter, install the vo Python package from https://www.stsci.edu/trac/ssb/astrolib/
+# (also referred to as votable2recarray). I used vo-0.5.tar.gz. Then this script can be
+# run for the rest.
+#
+# Ludwig Schwardt
+# 12 March 2010
+#
+
+from __future__ import with_statement
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+from vo.table import parse_single_table
+
+# For each flux field in table, specify the name, centre frequency and start frequency (in MHz)
+flux_bins = ['S80', 'S178', 'S408', 'S635', 'S1410', 'S2700', 'S5000', 'S8400', 'S22000']
+freq = np.array([80.0, 178.0, 408.0, 635.0, 1410.0, 2700.0, 5000.0, 8400.0, 22000.0])
+start = [20.0, 100.0, 200.0, 400.0, 750.0, 1500.0, 3000.0, 6000.0, 12000.0, 30000.0]
+# List of anomalous flux fields that will be edited out
+anomalies = {'J0108+1320' : 6, 'J0541-0154' : 2, 'J2253+1608' : 6}
+
+# Load main table in one shot
+table = parse_single_table("pkscat90_S1410_min_10Jy.vot")
+
+# Fit all sources on one figure
+plt.figure(1)
+plt.clf()
+plot_rows = int(np.ceil(np.sqrt(table.nrows)))
+src_strings = []
+
+# Iterate through sources
+for n, src in enumerate(table.array):
+    names = src['Jname']
+    if len(src['Alias']) > 0:
+        names += ' | *' + src['Alias']
+    tags = 'radec J2000'
+    ra = src['RA2000'].strip().replace(' ', ':')
+    dec = src['DE2000'].strip().replace(' ', ':')
+    flux = np.array([src[bin] for bin in flux_bins])
+    # Edit out anomalous flux bins for specific sources to improve fit (these are outside KAT7 frequency band anyway)
+    anomalous_flux, anomalous_bin = np.nan, -1
+    if src['Jname'] in anomalies:
+        anomalous_bin = anomalies[src['Jname']]
+        anomalous_flux = flux[anomalous_bin]
+        flux[anomalous_bin] = np.nan
+    # Fit Baars 1977 polynomial flux model: log10 S[Jy] = a + b*log10(f[MHz]) + c*(log10(f[MHz]))^2
+    flux_defined = ~np.isnan(flux)
+    log_freq = np.log10(freq[flux_defined])
+    log_flux = np.log10(flux[flux_defined])
+    flux_poly = np.polyfit(log_freq, log_flux, 2 if len(log_flux) > 3 else 1 if len(log_flux) > 1 else 0)
+    # Determine widest possible frequency range where flux is defined (ignore internal gaps in this range)
+    defined_bins = flux_defined.nonzero()[0]
+    freq_range = [start[defined_bins[0]], start[defined_bins[-1] + 1]]
+    flux_str = '(%g %g %s)' % (freq_range[0], freq_range[1], ' '.join(['%.4g' % (coef,) for coef in flux_poly[::-1]]))
+    src_strings.append(', '.join((names, tags, ra, dec, flux_str)) + '\n')
+    print src_strings[-1].strip()
+
+    # Display flux polynomial fits
+    test_log_freq = np.linspace(np.log10(start[0]), np.log10(start[-1]), 200)
+    test_log_flux = np.polyval(flux_poly, test_log_freq)
+    plt.subplot(plot_rows, plot_rows, n + 1)
+    plt.plot(log_freq, log_flux, 'ob')
+    plt.plot(test_log_freq, test_log_flux, 'r')
+    if not np.isnan(anomalous_flux):
+        plt.plot(np.log10(freq[anomalous_bin]), np.log10(anomalous_flux), '*b')
+    plt.xticks([]); plt.yticks([])
+
+with file('parkes_source_list.csv', 'w') as f:
+    f.writelines(src_strings)
