390b92e66d5bdcf75cb78fb27fde552a1209d093
Ludwig Schwardt <ludwig@ska.ac.za>
2010-04-16 15:50:02 +0200

    Added SVN revision numbers as version numbers for scape and katpoint, accessed via scape.__version__ and katpoint.__version__.
    git-svn-id: https://katfs.kat.ac.za/svnDS/code/katpoint/trunk@5131 b56c7a7d-ac47-0410-a905-b6f7de78896a

--- katpoint/__init__.py
+++ katpoint/__init__.py
@@ -52,3 +52,13 @@ class _NullHandler(_logging.Handler):
         pass
 logger = _logging.getLogger("katpoint")
 logger.addHandler(_NullHandler())
+
+try:
+    import pkg_resources as _pkg_resources
+    dist = _pkg_resources.get_distribution("katpoint")
+    # ver needs to be a list since tuples in Python <= 2.5 don't have
+    # a .index method.
+    ver = list(dist.parsed_version)
+    __version__ = "r%d" % int(ver[ver.index("*r") + 1])
+except (ImportError, _pkg_resources.DistributionNotFound, ValueError, IndexError, TypeError):
+    __version__ = "unknown"
