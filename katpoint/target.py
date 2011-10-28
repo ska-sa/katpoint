ska-sa/katpoint

commit fada2cc874d3dff0e684a6c5887d7f36c59979bb

(schwardt, tshaks) Vectorised the basic calculation of flux density, which resulted in an 80-fold speedup on 1024 channels. The flux model is now always evaluated first (whether for array or scalar frequencies), whereafter the frequency range check is done separately for arrays and scalars. This got rid of a slow if-statement within a function within a for-loop. The downside is that frequencies outside the range are evaluated slower (5x for scalars), as the flux model calculation is unnecessary in this case.
git-svn-id: https://katfs.kat.ac.za/svnDS/code/katpoint/trunk@11548 b56c7a7d-ac47-0410-a905-b6f7de78896a

katpoint/target.py
@@ -118,18 +118,15 @@ class FluxDensityModel(object):
 
         """
         a, b, c, d, e, f = self.coefs
-        def _scalar_flux_density(v):
-            """Calculate flux density S for a single frequency v in MHz."""
-            if (v < self.min_freq_MHz) or (v > self.max_freq_MHz):
-                # Frequency out of range for flux calculation of target
-                return np.nan
-            log10_v = np.log10(v)
-            log10_S = a + b*log10_v + c*log10_v**2 + d*log10_v**3 + e*np.exp(f*log10_v)
-            return 10 ** log10_S
+        log10_v = np.log10(freq_MHz)
+        log10_S = a + b*log10_v + c*log10_v**2 + d*log10_v**3 + e*np.exp(f*log10_v)
+        flux = 10 ** log10_S
         if is_iterable(freq_MHz):
-            return np.array([_scalar_flux_density(v) for v in freq_MHz])
+            flux[freq_MHz < self.min_freq_MHz] = np.nan
+            flux[freq_MHz > self.max_freq_MHz] = np.nan
+            return flux
         else:
-            return _scalar_flux_density(freq_MHz)
+            return flux if (freq_MHz >= self.min_freq_MHz) and (freq_MHz <= self.max_freq_MHz) else np.nan
 
 #--------------------------------------------------------------------------------------------------
 #--- CLASS :  Target

commit 7b986eb82e320ba61417541875d39b7609e1ca1d

Fixed bug introduced in previous commit (r11548), where specifying frequencies as a list of numbers led to all fluxes being NaN. Also added a new mini-feature that allows Targets to be constructed from existing Target objects (effectively a copy constructor).
git-svn-id: https://katfs.kat.ac.za/svnDS/code/katpoint/trunk@11549 b56c7a7d-ac47-0410-a905-b6f7de78896a

katpoint/target.py
@@ -122,6 +122,7 @@ class FluxDensityModel(object):
         log10_S = a + b*log10_v + c*log10_v**2 + d*log10_v**3 + e*np.exp(f*log10_v)
         flux = 10 ** log10_S
         if is_iterable(freq_MHz):
+            freq_MHz = np.asarray(freq_MHz)
             flux[freq_MHz < self.min_freq_MHz] = np.nan
             flux[freq_MHz > self.max_freq_MHz] = np.nan
             return flux
@@ -185,9 +186,9 @@ class Target(object):
 
     Parameters
     ----------
-    body : :class:`ephem.Body` object or string
+    body : :class:`ephem.Body` object or :class:`Target` object or string
         Pre-constructed PyEphem Body object to embed in target object, or
-        description string
+        existing target object or description string
     tags : list of strings, or whitespace-delimited string, optional
         Descriptive tags associated with target, starting with its body type
     aliases : list of strings, optional
@@ -211,6 +212,8 @@ class Target(object):
 
     """
     def __init__(self, body, tags=None, aliases=None, flux_model=None, antenna=None, flux_freq_MHz=None):
+        if isinstance(body, Target):
+            body = body.description
         # If the first parameter is a description string, extract the relevant target parameters from it
         if isinstance(body, basestring):
             body, tags, aliases, flux_model = construct_target_params(body)

katpoint/test/test_target.py
@@ -81,6 +81,7 @@ class TestTargetConstruction(unittest.TestCase):
         self.assertEqual(t1.description, t2.description, 'Target description string not updated')
         self.assertEqual(t1, t2.description, 'Equality with description string failed')
         self.assertEqual(t1, t2, 'Equality with target failed')
+        self.assertEqual(t1, katpoint.Target(t2), 'Construction with target object failed')
 
     def test_constructed_coords(self):
         """Test whether calculated coordinates match those with which it is constructed."""
@@ -132,6 +133,8 @@ class TestFluxDensityModel(unittest.TestCase):
         self.assertEqual(self.too_few_params.flux_density(1.5), 100.0, 'Flux calculation for too few params wrong')
         np.testing.assert_equal(self.flux_model.flux_density([1.5, 1.5]),
                                 np.array([100.0, 100.0]), 'Flux calculation for multiple frequencies wrong')
+        np.testing.assert_equal(self.flux_model.flux_density([0.5, 2.5]),
+                                np.array([np.nan, np.nan]), 'Flux calculation for out-of-range frequencies wrong')
         self.assertRaises(ValueError, self.no_flux_target.flux_density)
         np.testing.assert_equal(self.no_flux_target.flux_density([1.5, 1.5]),
                                 np.array([np.nan, np.nan]), 'Empty flux model leads to wrong empty flux shape')