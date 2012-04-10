d1b53ec9808e322d8b0e46a4780a601473b04972
Lize van den Heever <lvdheever@ska.ac.za>
2012-04-10 13:31:20 +0200

    Update jenkins build - no system-requirements.txt file
    git-svn-id: https://katfs.kat.ac.za/svnDS/code/katpoint/trunk@13263 b56c7a7d-ac47-0410-a905-b6f7de78896a

--- jenkins-build.sh
+++ jenkins-build.sh
@@ -1,8 +1,10 @@
 svn co https://katfs.kat.ac.za/svnDS/code/tools
-tools/kat_build_virtualenv.py -s checkout/system-requirements.txt -t venv
+#note: no -s checkout/system-requirements.txt
+tools/kat_build_virtualenv.py -t env
 . venv/bin/activate
 # Install other svn dependencies
 # pip install svn+https://katfs.kat.ac.za/svnDS/code/katcp-python/trunk@${SVN_REVISION}
 
 # Install self
 pip install ./checkout
+
