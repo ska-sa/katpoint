d3f4789b24623e22ea40ba084b3454fb3d967f81
Neilen Marais <nmarais@ska.ac.za>
2012-04-11 12:43:34 +0200

    Make katpoint build script use correct virtualenv name
    git-svn-id: https://katfs.kat.ac.za/svnDS/code/katpoint/trunk@13281 b56c7a7d-ac47-0410-a905-b6f7de78896a

--- jenkins-build.sh
+++ jenkins-build.sh
@@ -1,5 +1,5 @@
 svn co https://katfs.kat.ac.za/svnDS/code/tools
-tools/kat_build_virtualenv.py -s checkout/system-requirements.txt -t env
+tools/kat_build_virtualenv.py -s checkout/system-requirements.txt -t venv
 . venv/bin/activate
 # Install other svn dependencies
 # pip install svn+https://katfs.kat.ac.za/svnDS/code/katcp-python/trunk@${SVN_REVISION}
