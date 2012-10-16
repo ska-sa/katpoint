Fix to increase the accuracy in the calculation of UVW co-ordinates. The U vector is now calculated by precessing the J2000 coordinates of the north celestial pole to the epoch of observation and crossing the enu vector pointing to this position with the enu vector pointing at the source (previously the north celestial pole vector was defined at the epoch of observation). This change brings the UVW coordinates calculated in katpoint close to those calculted by common analysis packages such as CASA and AIPS. The new UVW coordinates should inprove the accuracy of the positions in KAT-7 images, although problems with source positions in images near the South Celestial Pole still remain and continue to be investigated......
git-svn-id: https://katfs.kat.ac.za/svnDS/code/katpoint/trunk@15585 b56c7a7d-ac47-0410-a905-b6f7de78896a

katpoint/target.py
@@ -711,12 +711,16 @@ class Target(object):
         timestamp, antenna = self._set_timestamp_antenna_defaults(timestamp, antenna)
         # Obtain baseline vector from reference antenna to second antenna
         baseline_m = antenna.baseline_toward(antenna2)
+        # NCP vector is J2000 NCP
+        ncp = construct_radec_target(0.0,np.pi/2)
+        # Get J2000 NCP az-el vector at current epoch pointed to by reference antenna
+        ncp_az,ncp_el = ncp.azel(timestamp, antenna)
         # Obtain direction vector(s) from reference antenna to target
         az, el = self.azel(timestamp, antenna)
         # w axis points toward target
         w = np.array(azel_to_enu(az, el))
-        # Vector pointing from reference antenna to north celestial pole
-        z = np.array([0.0, np.cos(antenna.observer.lat), np.sin(antenna.observer.lat)])
+        # enu vector pointing from reference antenna to north celestial pole
+        z = np.array(azel_to_enu(ncp_az, ncp_el))
         # u axis is orthogonal to z and w, and row_stack ensures it's 2-D array of column vectors
         u = np.row_stack(np.cross(z, w, axis=0))
         u_norm = np.sqrt(np.sum(u ** 2, axis=0))