4eb5d03cdcc91a46e172df843cddf35ed86b1622
Ludwig Schwardt <ludwig@ska.ac.za>
2014-02-06 22:03:25 +0200

    Refactor DelayModel to use Model as base

--- katpoint/delay.py
+++ katpoint/delay.py
@@ -9,78 +9,42 @@ import logging
 
 import numpy as np
 
+from .model import Parameter, Model
+
+
 logger = logging.getLogger(__name__)
 
 
-class DelayModel(object):
-    # Number of parameters in full antenna delay model
-    num_params = 6
-
-    __doc__ = """Model of the delay contribution from a single antenna.
+class DelayModel(Model):
+    """Model of the delay contribution from a single antenna.
 
     Parameters
     ----------
-    params : sequence of up to %d floats, or string, optional
-        Parameters of full model, in metres (defaults to sequence of zeroes).
-        If it is a string, it is interpreted as a whitespace-separated sequence
-        of parameters as produced by the :attr:`description` property
-        (ignoring commas). The parameters are assigned in Procrustean fashion:
-        select the first %d parameters of *params* or the entire *params*,
-        whichever is smallest, and set the unused parameters to zero
-        (useful to load old versions of the model).
+    model : file-like object, sequence of %d floats, or string, optional
+        Model specification. If this is a file-like object, load the model
+        from it. If this is a sequence of floats, accept it directly as the
+        model parameters (defaults to sequence of zeroes). If it is a string,
+        interpret it as a comma-separated (or whitespace-separated) sequence
+        of parameters in their string form (i.e. a description string).
 
-    """ % (num_params, num_params)
-    def __init__(self, params=None):
-        if params is None:
-            params = np.zeros(self.num_params)
-        elif isinstance(params, basestring):
-            params = np.array([float(p.strip(', ')) for p in params.split()])
-        params = np.asarray(params)
-        if len(params) < self.num_params:
-            padded = np.zeros(self.num_params)
-            padded[:len(params)] = params
-            params = padded
-        else:
-            discarded_actives = len(params[self.num_params:].nonzero()[0])
-            if discarded_actives:
-                logger.warning('Delay model received too many parameters (%d instead of %d), '
-                               'and %d non-zero parameters will be discarded' %
-                               (len(params), self.num_params, discarded_actives))
-            params = params[:self.num_params]
-        self.params = params
-
-    def __repr__(self):
-        """Short human-friendly string representation of delay model object."""
-        return "<katpoint.DelayModel active_params=%d/%d at 0x%x>" % \
-               (len(self.params.nonzero()[0]), self.num_params, id(self))
-
-    def __str__(self):
-        """Verbose human-friendly string representation of delay model object."""
-        num_active = len(self.params.nonzero()[0])
-        summary = "Delay model has %d parameters with %d active (non-zero)" % (self.num_params, num_active)
-        if num_active == 0:
-            return summary
-        descr = ['POS_E = %7.3f m (antenna position: offset East of reference position)',
-                 'POS_N = %7.3f m (antenna position: offset North of reference position)',
-                 'POS_U = %7.3f m (antenna position: offset above reference position)',
-                 'NIAO  = %7.3f m (non-intersecting axis offset - distance between az and el axes)',
-                 'CAB_H = %7.3f m (electronic path / cable length for H feed)',
-                 'CAB_V = %7.3f m (electronic path / cable length for V feed)']
-        param_strs = [(descr[p] % self.params[p]) for p in range(self.num_params) if self.params[p] != 0.0]
-        return summary + ':\n' + '\n'.join(param_strs)
-
-    def __eq__(self, other):
-        """Equality comparison operator."""
-        return self.description == (other.description if isinstance(other, DelayModel) else other)
-
-    def __ne__(self, other):
-        """Inequality comparison operator."""
-        return not (self == other)
-
-    @property
-    def description(self):
-        """String representation of delay model, sufficient to reconstruct it."""
-        return ', '.join([('%.4f' % self.params[p]) for p in xrange(self.num_params)])
+    """
+    def __init__(self, model=None):
+        # Instantiate the relevant model parameters and register with base class
+        params = []
+        params.append(Parameter('POS_E', 'm', 'antenna position: offset East of reference position'))
+        params.append(Parameter('POS_N', 'm', 'antenna position: offset North of reference position'))
+        params.append(Parameter('POS_U', 'm', 'antenna position: offset above reference position'))
+        params.append(Parameter('NIAO',  'm', 'non-intersecting axis offset - distance between az and el axes'))
+        params.append(Parameter('CAB_H', 'm', 'electronic path / cable length for H feed'))
+        params.append(Parameter('CAB_V', 'm', 'electronic path / cable length for V feed'))
+        Model.__init__(self, params)
+        self.set(model)
+        # Fix docstrings to contain the number of parameters
+        if '%d' in self.__class__.__doc__:
+            self.__class__.__doc__ = self.__class__.__doc__ % (len(self),)
+        # if '%d' in self.__class__.fit.im_func.__doc__:
+        #     self.__class__.fit.im_func.__doc__ = self.__class__.fit.im_func.__doc__ % \
+        #                                          (len(self), len(self))
 
 
 class CorrelatorDelays(object):
@@ -94,4 +58,3 @@ class CorrelatorDelays(object):
     def delays(self, target, t_start, t_next):
         """"""
         pass
-
