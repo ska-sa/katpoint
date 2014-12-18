baaaf4481e4e0ef669f71314b1f5f07ceefc232d
Ludwig Schwardt <ludwig@ska.ac.za>
2014-12-18 16:13:37 +0200

    Fix package import with missing pkg_resources
    If there is no pkg_resources, handle the ImportError but don't then
    also look for a pkg_resources exception...

--- katpoint/__init__.py
+++ katpoint/__init__.py
@@ -62,10 +62,14 @@ logger.addHandler(_no_config_handler)
 
 try:
     import pkg_resources as _pkg_resources
-    dist = _pkg_resources.get_distribution("katpoint")
-    # ver needs to be a list since tuples in Python <= 2.5 don't have
-    # a .index method.
-    ver = list(dist.parsed_version)
-    __version__ = "r%d" % int(ver[ver.index("*r") + 1])
-except (ImportError, _pkg_resources.DistributionNotFound, ValueError, IndexError, TypeError):
+except ImportError:
     __version__ = "unknown"
+else:
+    try:
+        dist = _pkg_resources.get_distribution("katpoint")
+        # ver needs to be a list since tuples in Python <= 2.5 don't have
+        # a .index method.
+        ver = list(dist.parsed_version)
+        __version__ = "r%d" % int(ver[ver.index("*r") + 1])
+    except (_pkg_resources.DistributionNotFound, ValueError, IndexError, TypeError):
+        __version__ = "unknown"
