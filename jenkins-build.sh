627c7ce7b9bff98ad75eba8f21567c8bfeb62498
Bruce Merry <bmerry@ska.ac.za>
2016-04-06 11:49:19 +0200

    Install test requirements

--- jenkins-build.sh
+++ jenkins-build.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 set -e -x
-pip install -U pip setuptools wheel
-pip install coverage
-install-requirements.py -d ~/docker-base/base-requirements.txt -r requirements.txt
-# install-requirements.py -d ~/docker-base/base-requirements.txt -r test-requirements.txt
+pip install -r ~/docker-base/pre-requirements.txt
+install-requirements.py -d ~/docker-base/base-requirements.txt -r requirements.txt -r test-requirements.txt
 nosetests --with-xunit --with-coverage --cover-erase --cover-xml --cover-inclusive --cover-package=katpoint
