1fa4fcf5e2b5d0bfaac00e7050e71b53ccc6713f
Bruce Merry <bmerry@ska.ac.za>
2021-03-08 12:32:28 +0200

    Remove unused file

--- jenkins-build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -e -x
-pip install -r ~/docker-base/pre-requirements.txt
-pip install -r requirements.txt -r test-requirements.txt
-nosetests --with-xunit --with-coverage --cover-erase --cover-xml --cover-inclusive --cover-package=katpoint
